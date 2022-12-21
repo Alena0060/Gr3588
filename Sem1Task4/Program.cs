a3957825417b87d2c157f2ab7409195e4e5df4cf
Alena <alena22ptt@gmail.com>
2022-12-21 23:07:49 +0700

    add solution for task 2,4,6,8

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2 3 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("enter the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter the third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    if (firstNumber > thirdNumber)
+    {
+        Console.WriteLine("maximum number: " + firstNumber);
+    }
+    else
+    {
+        Console.WriteLine("maximum number: " + thirdNumber);
+        
+    }
+}
+
+else if (secondNumber > thirdNumber)
+{
+    Console.WriteLine("maximum number: " + secondNumber);
+}
+else
+{
+    Console.WriteLine("maximum number: " + thirdNumber);
+}
