d35c18c5d2b22027dc49bb8a0fa41609b60f9a21
Alena <alena22ptt@gmail.com>
2022-12-21 21:51:53 +0700

    add solution for task 0,1,3,5,7

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// Считываем даннфе с консоли
+string? inputLine = Console.ReadLine();
+
+// Проверяем, чтобы данные были не пустыми
+if(inputLine!=null)
+{
+//Создаем массив с днями недели
+string[] dayOfWeek = new string[7];
+dayOfWeek[0] = "Monday";
+dayOfWeek[1] = "Tuesday";
+dayOfWeek[2] = "Wednesday";
+dayOfWeek[3] = "Thursday";
+dayOfWeek[4] = "Friday";
+dayOfWeek[5] = "Saturday";
+dayOfWeek[6] = "Sunday";
+
+//Парсим введеное число
+int inputNumber = int.Parse(inputLine);
+
+//находим название дня недели по введеному номеру
+string outDayOfWeek = dayOfWeek[inputNumber-1];
+
+//Выводим данные в консоль
+Console.WriteLine(outDayOfWeek);
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+// Проверяем чтобы данные были не пустыми
+if (inputLine != null)
+{
+
+    // Парсим введеное число
+    int inputNumber = int.Parse(inputLine);
+
+    int starNumber = inputNumber * (-1);
+
+    //Выходное значение
+    string outLine = string.Empty;
+
+    while(starNumber<inputNumber)
+    {
+        outLine = outLine + starNumber + ",";
+        starNumber++;
+        // starNumber = starNumber+1;
+    }
+    outLine = outLine + inputNumber;
+
+    //выводим данные в консоль
+    Console.WriteLine(outLine);
+}
