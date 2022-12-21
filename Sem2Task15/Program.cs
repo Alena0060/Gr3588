4e863247e39e66292c39495c29bc4c34aa172eaf
Alena <alena22ptt@gmail.com>
2022-12-22 03:01:56 +0700

    add solution for task 10,13,15

---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+int dayNumber = ReadInt("Enter a number from 1 to 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция принимает число от 1 до 7 и выводит сообщение - выходной день или нет.
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("The number " + a + " - Weekend");
+        }
+        else
+        {
+            Console.Write("The number " + a + " - Working ");
+        }
+    }
+    else
+    {
+        Console.Write("the number is not in the range from 1 to 7");
+    }
+    return " day.";
+}
