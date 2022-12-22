e51c0a4ce3381c6f331c5e2b9023a3893a98f545
Alena <alena22ptt@gmail.com>
2022-12-23 01:38:04 +0700

    add for task solution 19

--- Sem3Task19/Program.cs
+++ Sem3Task19/Program.cs
@@ -3,22 +3,52 @@
 //12821 -> да
 //23432 -> да
 
-Console.WriteLine("input number: ");
-string number = Console.ReadLine();
-int len = number.Length;
+//Console.WriteLine("input number: ");
+//string number = Console.ReadLine();
+//int len = number.Length;
 
-if (len == 5)
+//if (len == 5)
+//{
+//    if (number[0] == number[4] && number[1] == number[3])
+//    {
+//        Console.WriteLine($"{number} - Palindrome");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{number} - not a palindrome");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine($"mistake: {number} - is not a five - digit");
+//}
+
+
+Console.Clear();
+ 
+void CheckPalindromicNumber(int number)
 {
-    if (number[0] == number[4] && number[1] == number[3])
+    if(number >= 10000)
     {
-        Console.WriteLine($"{number} - Palindrome");
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = (number / 100) % 10;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Yes");
+            }
+            else 
+            Console.WriteLine("No");
+            
     }
     else
-    {
-        Console.WriteLine($"{number} - not a palindrome");
-    }
+    Console.WriteLine("Invalid number!");
 }
-else
-{
-    Console.WriteLine($"mistake: {number} - is not a five - digit");
-}
+ 
+Console.WriteLine("Enter a five-digit number:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
